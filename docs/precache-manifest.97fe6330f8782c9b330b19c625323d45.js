a12b2aa4fd9fc2fed5796541495f8eb3803a97d1
Maria Violeta <mariavioleta1417@gmail.com>
2018-11-04 07:39:35 +0100

    add pages

---
+++ docs/precache-manifest.97fe6330f8782c9b330b19c625323d45.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f85e4bfe865defa18588fc8e8448611",
+    "url": "./static/media/raleway-v12-latin-900.0f85e4bf.woff2"
+  },
+  {
+    "revision": "16315ca9591f7b69aeae",
+    "url": "./static/css/main.a6086667.chunk.css"
+  },
+  {
+    "revision": "d66c358f471c99250008",
+    "url": "./static/js/1.d66c358f.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "9d082dd652dec2c7474c352559c3dab8",
+    "url": "./static/media/play-webtv.9d082dd6.svg"
+  },
+  {
+    "revision": "827182817f267a45d66d5505e4ea65ad",
+    "url": "./static/media/raleway-v12-latin-500.82718281.woff"
+  },
+  {
+    "revision": "63f872bd86b6c89b54a78a5ff8f8c634",
+    "url": "./static/media/raleway-v12-latin-500.63f872bd.ttf"
+  },
+  {
+    "revision": "b1739e04c0f6cb3e23a4d4d50f3952d8",
+    "url": "./static/media/raleway-v12-latin-500.b1739e04.eot"
+  },
+  {
+    "revision": "8f38ea72739afb910bc6ce24bf44ddd6",
+    "url": "./static/media/raleway-v12-latin-900.8f38ea72.eot"
+  },
+  {
+    "revision": "a8c8ed48a1db3d40bda84eda823b4c14",
+    "url": "./static/media/raleway-v12-latin-500.a8c8ed48.svg"
+  },
+  {
+    "revision": "16315ca9591f7b69aeae",
+    "url": "./static/js/main.16315ca9.chunk.js"
+  },
+  {
+    "revision": "a58f25dc87faec123dd985c60acd54ff",
+    "url": "./static/media/raleway-v12-latin-900.a58f25dc.woff"
+  },
+  {
+    "revision": "e4fad40c810440a33c571feb566c8f25",
+    "url": "./static/media/raleway-v12-latin-900.e4fad40c.ttf"
+  },
+  {
+    "revision": "de818060c850c7842e9f2cb4d409d2ba",
+    "url": "./static/media/raleway-v12-latin-500.de818060.woff2"
+  },
+  {
+    "revision": "c5d97cc0ba2d84cca401311cc004b548",
+    "url": "./static/media/raleway-v12-latin-900.c5d97cc0.svg"
+  },
+  {
+    "revision": "07cfcad17571b4dd13fa2dbe73880e1a",
+    "url": "./static/media/photo1.07cfcad1.png"
+  },
+  {
+    "revision": "85c981201dd1dc8326c473df5d371692",
+    "url": "./static/media/photo2.85c98120.png"
+  },
+  {
+    "revision": "2f5b38ef91c63d282b375475b3dfb3ee",
+    "url": "./static/media/photo3.2f5b38ef.png"
+  },
+  {
+    "revision": "9c710c978aa227a4c561823d324457e3",
+    "url": "./static/media/photo5.9c710c97.png"
+  },
+  {
+    "revision": "fd9efee90e497e8bfa209b55f6dfb5a1",
+    "url": "./static/media/photo4.fd9efee9.png"
+  },
+  {
+    "revision": "24511690cc9b043b01d1c3c73cd40802",
+    "url": "./index.html"
+  }
+];
