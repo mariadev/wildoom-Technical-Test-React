590cafce518e394e92b2316b05c936d44c6d8331
Maria Violeta <mariavioleta1417@gmail.com>
2018-11-05 19:52:04 +0100

    add github pages

--- src/components/modules/InnerTextGridCategory.js
+++ src/components/modules/InnerTextGridCategory.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 class InnerTextCategory extends Component {
     render() {
         return (
-                <h2 className="wrapper-text-grid__category">
+                <h4 className="wrapper-text-grid__category">
                     {this.props.category}
-                </h2>
+                </h4>
         );
     }
 }
